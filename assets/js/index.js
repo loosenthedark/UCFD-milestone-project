6c5c1bd3d9e4ddccd3bae8ec585fcd01da244b34
Ubuntu <ubuntu@ip-172-31-91-29.ec2.internal>
2019-08-02 22:56:32 +0000

    Fixed smooth scrolling/carousel controls bug

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,24 @@
 
 /* JavaScript/jQuery code below adapted from the following JSFiddle demo: https://jsfiddle.net/9SDLw/ */
 
-$('a').click(function(){
+/* $('a').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 800);
     return false;
+}); */
+
+/* jQuery smoothscroll workaround implemented via Traversy Media c/o the following YouTube tutorial: https://www.youtube.com/watch?v=y9nlfqT4s9s */
+
+$('a').on('click', function(e) {
+    if (this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body')
+            .animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+    }
 });
